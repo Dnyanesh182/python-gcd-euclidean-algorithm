41def493d5d466813eb46c498cfe77003757e238
Dnyaneshwar Potdar <dnyaneshwarpotdar2732001@gmail.com>
2026-03-15 16:59:06 +0530

    feat(gcd): handle edge case when one input number is zero in euclidean algorithm

--- gcdEuclideanAlgorithm.py
+++ gcdEuclideanAlgorithm.py
@@ -1,16 +1,22 @@
-# UC3 – Implement Euclidean Algorithm Logic
+# UC4 – Handle Case When One Number is Zero
 
 def calculate_gcd(a, b):
+
+    if a == 0:
+        return b
+
+    if b == 0:
+        return a
+
     while b != 0:
-        remainder = a % b
-        a = b
-        b = remainder
+        a, b = b, a % b
+
     return a
 
 
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
-result = calculate_gcd(num1, num2)
+gcd = calculate_gcd(num1, num2)
 
-print("GCD is:", result)
+print("GCD is:", gcd)
