b1ef65d719878fa02c3b241b92bc3461442af618
Dnyaneshwar Potdar <dnyaneshwarpotdar2732001@gmail.com>
2026-03-15 16:51:44 +0530

    feat(gcd): implement function to compute gcd using euclidean algorithm

--- gcdEuclideanAlgorithm.py
+++ gcdEuclideanAlgorithm.py
@@ -1,7 +1,14 @@
-# UC1 – Take Two Integer Inputs from User
+# UC2 – Define a Function to Calculate GCD
+
+def calculate_gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
 
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
-print("First Number:", num1)
-print("Second Number:", num2)
+gcd = calculate_gcd(num1, num2)
+
+print("GCD of", num1, "and", num2, "is:", gcd)
